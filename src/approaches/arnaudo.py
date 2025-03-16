ae74adb058c8ba42ecfd5c0346e4a5c5e43b3129
JY-Cheng <chengjiaying.prc@gmail.cpm>
2025-03-16 15:22:55 +0100

    update arnaudo.py

--- src/approaches/arnaudo.py
+++ src/approaches/arnaudo.py
@@ -1,6 +1,17 @@
 import pandas as pd
 import numpy as np
 
-def charging():
-    pass
+class Arnaudo():
+    def __init__(self, start_charging, stop_charging, start_discharging):
+        self.start_charging = start_charging
+        self.stop_charging = stop_charging
+        self.start_discharging = start_discharging
+
+    def charging(self):
+        """
+        if SoC < 50%, start charging, until 90% stop
+        if SoC > 80%, start discharging, until 80% stop
+        :return:
+        """
+        pass
 
