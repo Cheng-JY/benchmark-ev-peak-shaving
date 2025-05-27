6a7530a6c329e2057161b879f12e4935b8566f6e
jcheng <jiaying.cheng@iee.fraunhofer.de>
2025-05-27 14:11:44 +0200

    update wang

--- src/approaches/_wang.py
+++ src/approaches/_wang.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from typing import Dict, Tuple, Optional
+from src.utils._calculation import calculate_SoC
 from ..base import AggregateCoordinateStrategy
 
 class Wang(AggregateCoordinateStrategy):
@@ -13,80 +13,75 @@ class Wang(AggregateCoordinateStrategy):
             SoC_init: float,
             consumption: np.ndarray,
             target_load_ev: np.ndarray,
+            charging_status_quo: np.ndarray,
             SoC_max: np.ndarray,
-            SoC_min: np.ndarray,
-            charging_status_quo: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+            SoC_min: np.ndarray) -> np.ndarray:
         SoC = np.zeros(len(ch_avail))
         SoC[0] = SoC_init
         a = np.zeros(len(ch_avail))
         b = np.zeros(len(ch_avail))
-        y = np.zeros(len(ch_avail))
         charging = np.zeros(len(ch_avail))
-        feasible = np.ones(len(ch_avail), dtype=bool)
-        diff_ev_load = charging_status_quo - target_load_ev
 
-        for t in range(len(ch_avail)):
+        for t in range(len(ch_avail)-1):
             a[t] = np.minimum(SoC_max[t] - SoC[t], ch_avail[t])
             b[t] = np.minimum(SoC[t] - SoC_min[t], ch_avail[t])
-
-            if diff_ev_load[t] == 0:
-                y[t] = 0
-            elif diff_ev_load[t] > 0:
-                # need discharging
-                y[t] = (
-                    -np.abs(diff_ev_load[t])
-                    if np.abs(diff_ev_load[t]) < b[t]
+            diff = charging_status_quo[t] - target_load_ev[t]
+            if np.isclose(diff, 0.0, atol=1e-7):
+                charging[t] = charging_status_quo[t]
+            elif diff > 0:
+                charging[t] = (
+                    -np.minimum(np.abs(target_load_ev[t]), ch_avail[t])
+                    if np.abs(target_load_ev[t]) < b[t]
                     else -b[t]
                 )
-            elif np.abs(diff_ev_load[t]) < a[t]:
-                # need charging
-                y[t] = np.abs(target_load_ev[t])
             else:
-                y[t] = a[t]
-            charging[t] = y[t] + charging_status_quo[t]
+                # target_load_ev[t] < 0
+                if np.abs(diff) < a[t]:
+                    charging[t] = np.minimum(np.abs(target_load_ev[t]), ch_avail[t])
+                else:
+                    charging[t] = np.minimum(a[t], ch_avail[t])
             SoC[t+1] = np.maximum(SoC[t] + charging[t] - consumption[t], 0)
-
+        
         return SoC, charging
 
     def get_coordinate_SoC_charging(self,
             threshold:float,
             household_load: pd.DataFrame,
-            charging_status_quo: Dict[str, np.ndarray],
-            soc_status_quo: Dict[str, np.ndarray],
-            soc_max: Dict[str, np.ndarray],
-            soc_min: Dict[str, np.ndarray],
-            ch_avail: Dict[str, np.ndarray],
-            consumption: Dict[str, np.ndarray]
-    ) -> Tuple[Dict[str, np.ndarray], Dict[str, np.ndarray], Dict[str, np.ndarray], Dict[str, np.ndarray]]:
-        # handling uncontrolled part
-        adjusted_threshold = threshold - household_load
+            charging_status_quo: dict[str, np.ndarray],
+            soc_status_quo: dict[str, np.ndarray],
+            soc_max: dict[str, np.ndarray],
+            soc_min: dict[str, np.ndarray],
+            ch_avail: dict[str, np.ndarray],
+            consumption: dict[str, np.ndarray]):
+        
+        threshold = threshold - household_load
 
-        soc_updated_dict = {"uncontrolled": soc_status_quo.get("uncontrolled", np.array([0]))}
-        charging_updated_dict = {"uncontrolled": charging_status_quo.get("uncontrolled", np.array([0]))}
-        feasible_dict = {
-            "uncontrolled": (charging_status_quo.get("uncontrolled", np.array([0])) < adjusted_threshold).astype(bool)}
-        threshold_dict = {"uncontrolled": adjusted_threshold}
+        charging_updated_dict = {}
+        feasible_dict = {}
+        threshold_dict = {}
 
-        uncontrolled_charging = charging_status_quo.get("uncontrolled", np.zeros_like(adjusted_threshold))
-        threshold_dict["controlled"] = adjusted_threshold - uncontrolled_charging
+        soc_updated_dict = {"uncontrolled": soc_status_quo.get("uncontrolled", 0)}
+        charging_updated_dict["uncontrolled"] = charging_status_quo.get("uncontrolled", 0)
+        feasible_dict["uncontrolled"] = charging_status_quo.get("uncontrolled") < threshold
+        threshold_dict["uncontrolled"] = threshold
+        threshold_dict["controlled_v2g"] = threshold_dict["uncontrolled"] - charging_updated_dict["uncontrolled"]
 
-        soc_init = soc_max["controlled"][0]
-        soc_controlled, charging_controlled = self.get_SoC_charging(
-            ch_avail["controlled"],
-            soc_init,
-            consumption["controlled"],
-            threshold_dict["controlled"],
-            soc_max["controlled"],
-            soc_min["controlled"],
-            charging_status_quo["controlled"],
+        soc_init = soc_max.get("controlled_v2g")[0]
+        SOC_updated, charging_updated = self.get_SoC_charging(
+            ch_avail=ch_avail.get("controlled_v2g"),
+            SoC_init=soc_init,
+            charging_status_quo=charging_status_quo.get("controlled_v2g"),
+            target_load_ev=threshold_dict.get("controlled_v2g"),
+            consumption=consumption.get("controlled_v2g"),
+            SoC_max=soc_max.get("controlled_v2g"),
+            SoC_min=soc_min.get("controlled_v2g"),
         )
-        soc_updated_dict["controlled"] = soc_controlled
-        charging_updated_dict["controlled"] = charging_controlled
-
-        feasible_dict["controlled"] = (charging_controlled <= threshold_dict["controlled"]).astype(bool)
+        soc_updated_dict["controlled_v2g"] = SOC_updated
+        charging_updated_dict["controlled_v2g"] = charging_updated
+        feasible_dict["controlled_v2g"] = charging_updated < threshold_dict["controlled_v2g"]
+        threshold_dict["controlled_v2g"] = threshold
 
         return soc_updated_dict, charging_updated_dict, feasible_dict, threshold_dict
-
 
             
                 
